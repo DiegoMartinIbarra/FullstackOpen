eb71b12bd7395773d5a4058e8181ef8dee46276c
Diego Ibarra <diego12el@hotmail.com>
2022-07-24 21:08:55 -0300

    Agrego funcionalidad filtrado de elementos

--- part2/la_guia/src/index.js
+++ part2/la_guia/src/index.js
@@ -5,13 +5,15 @@ import { Names } from './components/Names';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: 59898527668 }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
-
   const [ newNumber, setNewNumber ] = useState('')
-
-
+  const [filter, setFilter] = useState('')
+  
   const addPerson = (event) => {
     event.preventDefault()
 
@@ -45,16 +47,33 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const serchfilter = (element) => {
+    var resultElements = persons.filter((persons) =>{
+      if(persons.name.toString().toLowerCase().includes(element.toLowerCase()) ){
+        return persons;
+      }
+    });
+    return resultElements;
+  }
+
+  let printName = serchfilter(filter);
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter shown with <input value={filter} onChange={handleFilter} /> </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div> name: <input value={newName} onChange={handleNewPerson} /> </div>
         <div> number: <input value={newNumber} onChange={handleNewNumber} /> </div>
         <div> <button type="submit">add</button> </div>
       </form>
       <h2>Numbers</h2>
-      { persons.map(persons => <Names key={persons.name} persons={persons} /> ) }
+      { filter.length === 0 ? persons.map(persons => <Names key={persons.name} persons={persons} />) :  printName.map(persons => <Names key={persons.name} persons={persons} />)}
       ...
     </div>
   )}
